8c9c0a18268bd0c011ca8af1f3a7f69bc397ac1f
Markmunyao <markkusu254@gmail.com>
2024-03-04 07:53:18 -0800

    first commit

---
+++ Datatypes.py
@@ -0,0 +1,37 @@
+a=100
+b=32
+c=3.14
+#print(a+b*2)
+#print((a+b)*2)
+#print(a==b)
+#print(a>b)
+d=12+2j
+#print(a+b+c+d)
+plp_str="welcome to plp academy"
+#print(plp_str)
+#print(plp_str[0])
+#print(plp_str[-1])
+#print(plp_str[6:9])
+#print(plp_str[13:15])
+student_str="python class"
+#print(plp_str + student_str)
+academy_ls= ["jack",30,3,4.2,5+7j,"thika"]
+student_ls=[30,5+7j,]
+print(academy_ls)
+print(academy_ls[0])
+print(academy_ls [4:6])
+print(academy_ls+student_ls)
+print(academy_ls*5)
+# A LIST OF THREE NUMBERS
+NUMBER=[1,2,3,"KUSU,4"]
+print(NUMBER)
+#ACCESS THE TERM AT INDEX O
+#print(NUMBER[0])
+#print(NUMBER[3])
+NUMBER.append(33)
+print("AFTER APPEND:",NUMBER)
+NUMBER[3]="KUCHI"
+print(NUMBER)
+NUMBER.remove("KUCHI")
+print(NUMBER)
+
